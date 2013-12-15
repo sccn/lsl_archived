1b8370ccd4eff9c567c72e60765c94f89c5026ca
chkothe <christiankothe@gmail.com>
2013-12-14 18:09:35 -0800

    Added support for new models of SMI eye trackers via the iViewNG SDK (particularly the eye tracking glasses). Renamed the SMI directory structure to better reflect the actual names of the API versions.

---
+++ Apps/SMI Eyetracker/iViewX-SDK/mainwindow.cpp
@@ -0,0 +1,258 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/foreach.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+const int num_channels = 13;					// hard-coded # of channels
+boost::shared_ptr<lsl::stream_outlet> outlet_;  // current stream outlet if linked (should be a member of MainWindow, but isn't due to the CBF)
+
+
+MainWindow::MainWindow(QWidget *parent, const std::string &config_file) :
+QMainWindow(parent), ui(new Ui::MainWindow) {
+	ui->setupUi(this);
+
+	// set logger 
+	iV_SetLogger(1, "iViewX-logfile.txt");
+
+	// parse startup config file
+	load_config(config_file);
+
+	// make GUI connections
+	QObject::connect(ui->actionQuit, SIGNAL(triggered()), this, SLOT(close()));
+	QObject::connect(ui->linkButton, SIGNAL(clicked()), this, SLOT(link_iviewx()));
+	QObject::connect(ui->actionLoad_Configuration, SIGNAL(triggered()), this, SLOT(load_config_dialog()));
+	QObject::connect(ui->actionSave_Configuration, SIGNAL(triggered()), this, SLOT(save_config_dialog()));
+}
+
+
+void MainWindow::load_config_dialog() {
+	QString sel = QFileDialog::getOpenFileName(this,"Load Configuration File","","Configuration Files (*.cfg)");
+	if (!sel.isEmpty())
+		load_config(sel.toStdString());
+}
+
+void MainWindow::save_config_dialog() {
+	QString sel = QFileDialog::getSaveFileName(this,"Save Configuration File","","Configuration Files (*.cfg)");
+	if (!sel.isEmpty())
+		save_config(sel.toStdString());
+}
+
+void MainWindow::closeEvent(QCloseEvent *ev) {
+	if (outlet_)
+		ev->ignore();
+}
+
+void MainWindow::load_config(const std::string &filename) {
+	using boost::property_tree::ptree;
+	ptree pt;
+
+	// parse file
+	try {
+		read_xml(filename, pt);
+	} catch(std::exception &e) {
+		QMessageBox::information(this,"Error",(std::string("Cannot read config file: ")+= e.what()).c_str(),QMessageBox::Ok);
+		return;
+	}
+
+	// get config values
+	try {
+		ui->serverAddress->setText(pt.get<std::string>("server.address","127.0.0.1").c_str());
+		ui->sendPort->setValue(pt.get<int>("local.sendport",4444));
+		ui->listenAddress->setText(pt.get<std::string>("local.address","127.0.0.1").c_str());
+		ui->receivePort->setValue(pt.get<int>("local.receiveport",5555));
+	} catch(std::exception &) {
+		QMessageBox::information(this,"Error in Config File","Could not read out config parameters.",QMessageBox::Ok);
+		return;
+	}
+}
+
+void MainWindow::save_config(const std::string &filename) {
+	using boost::property_tree::ptree;
+	ptree pt;
+
+	// transfer UI content into property tree
+	try {
+		pt.put("server.address",ui->serverAddress->text().toStdString());
+		pt.put("local.sendport",ui->sendPort->value());
+		pt.put("local.address",ui->listenAddress->text().toStdString());
+		pt.put("local.receiveport",ui->receivePort->value());
+	} catch(std::exception &e) {
+		QMessageBox::critical(this,"Error",(std::string("Could not prepare settings for saving: ")+=e.what()).c_str(),QMessageBox::Ok);
+	}
+
+	// write to disk
+	try {
+		write_xml(filename, pt);
+	} catch(std::exception &e) {
+		QMessageBox::critical(this,"Error",(std::string("Could not write to config file: ")+=e.what()).c_str(),QMessageBox::Ok);
+	}
+}
+
+// start/stop the iViewX connection
+void MainWindow::link_iviewx() {
+	if (outlet_) {
+		// === perform unlink action ===
+		try {
+			iV_SetSampleCallback(NULL);
+			iV_SetTrackingMonitorCallback(NULL);
+			iV_Disconnect();
+			outlet_.reset();
+		} catch(std::exception &e) {
+			QMessageBox::critical(this,"Error",(std::string("Could not stop the background processing: ")+=e.what()).c_str(),QMessageBox::Ok);
+			return;
+		}
+		// indicate that we are now successfully unlinked
+		ui->linkButton->setText("Link");
+	} else {
+		// === perform link action ===
+		try {
+			// get the UI parameters...
+			std::string serverAddress = ui->serverAddress->text().toStdString();
+			int sendPort = ui->sendPort->value();
+			std::string listenAddress = ui->listenAddress->text().toStdString();
+			int receivePort = ui->receivePort->value();
+
+			// connect to iViewX 
+			int ret_connect = iV_Connect((char*)serverAddress.c_str(), sendPort, (char*)listenAddress.c_str(), receivePort);
+
+			switch(ret_connect) {
+			case RET_SUCCESS:
+				break; // all fine
+			case ERR_COULD_NOT_CONNECT:
+				throw std::runtime_error("Connection could not be established");
+			case ERR_WRONG_PARAMETER:
+				throw std::runtime_error("Wrong Parameter used");
+			default:
+				throw std::runtime_error("Some unknown error occurred");
+			}
+
+			// read out tracker meta-data
+			SystemInfoStruct systemInfoData; 
+			iV_GetSystemInfo(&systemInfoData);
+			char *ETDeviceStrings[] = {"NONE", "RED", "REDm", "HiSpeed", "MRI", "HED", "ETG", "Custom", "unknown", "unknown", "unknown", "unknown", "unknown", "unknown", "unknown", "unknown"};
+			bool is_RED = std::string(ETDeviceStrings[systemInfoData.iV_ETDevice]).find("RED") != std::string::npos;
+
+			// create streaminfo
+			lsl::stream_info info("iViewX","Gaze",num_channels,systemInfoData.samplerate,lsl::cf_float32,"iViewX_" + serverAddress + boost::lexical_cast<std::string>(sendPort) + "_s6786sdf45");
+
+			// append some (optional) meta-data
+			lsl::xml_element channels = info.desc().append_child("channels");
+			channels.append_child("channel")
+				.append_child_value("label","Screen_X_left")
+				.append_child_value("eye","left")
+				.append_child_value("type","ScreenX")
+				.append_child_value("unit","pixels");
+			channels.append_child("channel")
+				.append_child_value("label","Screen_Y_left")
+				.append_child_value("eye","left")
+				.append_child_value("type","ScreenY")
+				.append_child_value("unit","pixels");
+			channels.append_child("channel")
+				.append_child_value("label","Screen_X_right")
+				.append_child_value("eye","right")
+				.append_child_value("type","ScreenX")
+				.append_child_value("unit","pixels");
+			channels.append_child("channel")
+				.append_child_value("label","Screen_Y_right")
+				.append_child_value("eye","right")
+				.append_child_value("type","ScreenY")
+				.append_child_value("unit","pixels");
+
+			channels.append_child("channel")
+				.append_child_value("label","Diameter_left")
+				.append_child_value("eye","left")
+				.append_child_value("type","Diameter")
+				.append_child_value("unit",is_RED?"millimeters":"pixels");
+			channels.append_child("channel")
+				.append_child_value("label","Diameter_right")
+				.append_child_value("eye","right")
+				.append_child_value("type","Diameter")
+				.append_child_value("unit",is_RED?"millimeters":"pixels");
+
+			channels.append_child("channel")
+				.append_child_value("label","EyePosition_X_left")
+				.append_child_value("eye","left")
+				.append_child_value("type","PositionX")
+				.append_child_value("unit","millimeters");
+			channels.append_child("channel")
+				.append_child_value("label","EyePosition_Y_left")
+				.append_child_value("eye","left")
+				.append_child_value("type","PositionY")
+				.append_child_value("unit","millimeters");
+			channels.append_child("channel")
+				.append_child_value("label","EyePosition_Z_left")
+				.append_child_value("eye","left")
+				.append_child_value("type","PositionZ")
+				.append_child_value("unit","millimeters");
+			channels.append_child("channel")
+				.append_child_value("label","EyePosition_X_right")
+				.append_child_value("eye","right")
+				.append_child_value("type","PositionX")
+				.append_child_value("unit","millimeters");
+			channels.append_child("channel")
+				.append_child_value("label","EyePosition_Y_right")
+				.append_child_value("eye","right")
+				.append_child_value("type","PositionY")
+				.append_child_value("unit","millimeters");
+			channels.append_child("channel")
+				.append_child_value("label","EyePosition_Z_right")
+				.append_child_value("eye","right")
+				.append_child_value("type","PositionZ")
+				.append_child_value("unit","millimeters");
+			channels.append_child("channel")
+				.append_child_value("label","PlaneNumber")
+				.append_child_value("type","Index");
+
+			info.desc().append_child("acquisition")
+				.append_child_value("manufacturer","SMI")
+				.append_child_value("model",ETDeviceStrings[systemInfoData.iV_ETDevice])
+				.append_child_value("tracker_version",(boost::lexical_cast<std::string>(systemInfoData.iV_MajorVersion) + "." + boost::lexical_cast<std::string>(systemInfoData.iV_MinorVersion) + "." + boost::lexical_cast<std::string>(systemInfoData.iV_Buildnumber)).c_str())
+				.append_child_value("API_version",(boost::lexical_cast<std::string>(systemInfoData.API_MajorVersion) + "." + boost::lexical_cast<std::string>(systemInfoData.API_MinorVersion) + "." + boost::lexical_cast<std::string>(systemInfoData.API_Buildnumber)).c_str());
+
+			// instantiate new outlet
+			outlet_.reset(new lsl::stream_outlet(info));
+
+			// setup callbacks
+			iV_SetSampleCallback(on_sample);
+
+		}
+		catch(std::exception &e) {
+			QMessageBox::critical(this,"Error",(std::string("Could not initialize the iViewX interface: ")+=e.what()).c_str(),QMessageBox::Ok);
+			return;
+		}
+
+		// done, all successful
+		ui->linkButton->setText("Unlink");
+	}
+}
+
+int __stdcall on_sample(SampleStruct s) {
+	if (!outlet_)
+		return 0;
+
+	float sample[num_channels] = {
+		s.leftEye.gazeX,s.leftEye.gazeY,
+		s.rightEye.gazeX,s.rightEye.gazeY,
+		s.leftEye.diam,s.rightEye.diam,
+		s.leftEye.eyePositionX,s.leftEye.eyePositionY,s.leftEye.eyePositionZ,
+		s.rightEye.eyePositionX,s.rightEye.eyePositionY,s.rightEye.eyePositionZ,
+		s.planeNumber};
+
+	// calc sample age
+	long long tracker_time = s.timestamp;
+	iV_GetCurrentTimestamp(&tracker_time);
+	double age = (tracker_time - s.timestamp)/1000000.0;
+	
+	// push into LSL
+	outlet_->push_sample(sample,lsl::local_clock()-age);
+	return 1;
+}
+
+MainWindow::~MainWindow() {
+	delete ui;
+}
---
+++ Apps/SMI Eyetracker/iViewX-SDK/ui_mainwindow.h
@@ -0,0 +1,209 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created: Sat Dec 14 17:32:26 2013
+**      by: Qt User Interface Compiler version 4.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QFormLayout>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QPushButton>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QSpinBox>
+#include <QtGui/QStatusBar>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionLoad_Configuration;
+    QAction *actionSave_Configuration;
+    QAction *actionQuit;
+    QAction *actionQuit_2;
+    QWidget *centralWidget;
+    QVBoxLayout *verticalLayout;
+    QFormLayout *formLayout;
+    QLabel *label_4;
+    QLineEdit *serverAddress;
+    QLabel *label_2;
+    QSpinBox *sendPort;
+    QLabel *label_5;
+    QLineEdit *listenAddress;
+    QLabel *label_3;
+    QSpinBox *receivePort;
+    QSpacerItem *verticalSpacer;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *linkButton;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(233, 191);
+        actionLoad_Configuration = new QAction(MainWindow);
+        actionLoad_Configuration->setObjectName(QString::fromUtf8("actionLoad_Configuration"));
+        actionSave_Configuration = new QAction(MainWindow);
+        actionSave_Configuration->setObjectName(QString::fromUtf8("actionSave_Configuration"));
+        actionQuit = new QAction(MainWindow);
+        actionQuit->setObjectName(QString::fromUtf8("actionQuit"));
+        actionQuit_2 = new QAction(MainWindow);
+        actionQuit_2->setObjectName(QString::fromUtf8("actionQuit_2"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        verticalLayout = new QVBoxLayout(centralWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        formLayout = new QFormLayout();
+        formLayout->setSpacing(6);
+        formLayout->setObjectName(QString::fromUtf8("formLayout"));
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        formLayout->setWidget(0, QFormLayout::LabelRole, label_4);
+
+        serverAddress = new QLineEdit(centralWidget);
+        serverAddress->setObjectName(QString::fromUtf8("serverAddress"));
+
+        formLayout->setWidget(0, QFormLayout::FieldRole, serverAddress);
+
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        formLayout->setWidget(1, QFormLayout::LabelRole, label_2);
+
+        sendPort = new QSpinBox(centralWidget);
+        sendPort->setObjectName(QString::fromUtf8("sendPort"));
+        sendPort->setMinimum(1);
+        sendPort->setMaximum(65535);
+        sendPort->setValue(3333);
+
+        formLayout->setWidget(1, QFormLayout::FieldRole, sendPort);
+
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+
+        formLayout->setWidget(2, QFormLayout::LabelRole, label_5);
+
+        listenAddress = new QLineEdit(centralWidget);
+        listenAddress->setObjectName(QString::fromUtf8("listenAddress"));
+
+        formLayout->setWidget(2, QFormLayout::FieldRole, listenAddress);
+
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        formLayout->setWidget(3, QFormLayout::LabelRole, label_3);
+
+        receivePort = new QSpinBox(centralWidget);
+        receivePort->setObjectName(QString::fromUtf8("receivePort"));
+        receivePort->setMinimum(1);
+        receivePort->setMaximum(65535);
+        receivePort->setValue(2222);
+
+        formLayout->setWidget(3, QFormLayout::FieldRole, receivePort);
+
+
+        verticalLayout->addLayout(formLayout);
+
+        verticalSpacer = new QSpacerItem(20, 4, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        linkButton = new QPushButton(centralWidget);
+        linkButton->setObjectName(QString::fromUtf8("linkButton"));
+
+        horizontalLayout->addWidget(linkButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 233, 18));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        MainWindow->setMenuBar(menuBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionLoad_Configuration);
+        menuFile->addAction(actionSave_Configuration);
+        menuFile->addSeparator();
+        menuFile->addAction(actionQuit_2);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "SMI iViewX Connector", 0, QApplication::UnicodeUTF8));
+        actionLoad_Configuration->setText(QApplication::translate("MainWindow", "Load Configuration", 0, QApplication::UnicodeUTF8));
+        actionSave_Configuration->setText(QApplication::translate("MainWindow", "Save Configuration", 0, QApplication::UnicodeUTF8));
+        actionQuit->setText(QApplication::translate("MainWindow", "Quit", 0, QApplication::UnicodeUTF8));
+        actionQuit_2->setText(QApplication::translate("MainWindow", "Quit", 0, QApplication::UnicodeUTF8));
+        label_4->setText(QApplication::translate("MainWindow", "iView X Server Address", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        serverAddress->setToolTip(QApplication::translate("MainWindow", "IP address of iView X computer", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        serverAddress->setText(QApplication::translate("MainWindow", "127.0.0.1", 0, QApplication::UnicodeUTF8));
+        label_2->setText(QApplication::translate("MainWindow", "Send Port", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        sendPort->setToolTip(QApplication::translate("MainWindow", "Port being used by this program for sending data to iView X", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        label_5->setText(QApplication::translate("MainWindow", "Local Listen Address", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        listenAddress->setToolTip(QApplication::translate("MainWindow", "IP address of local computer", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        listenAddress->setText(QApplication::translate("MainWindow", "127.0.0.1", 0, QApplication::UnicodeUTF8));
+        label_3->setText(QApplication::translate("MainWindow", "Receive Port", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        receivePort->setToolTip(QApplication::translate("MainWindow", "Port being used by this program for receiving data from iView X", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
+        linkButton->setText(QApplication::translate("MainWindow", "Link", 0, QApplication::UnicodeUTF8));
+        menuFile->setTitle(QApplication::translate("MainWindow", "File", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
