e6441db34fb9a9703ca4958a7805aaa30a727442
David Medine <dmedine@ucsd.edu>
2014-12-22 17:00:45 -0800

    Continuing to work on build environment for multiple OS. ClickME is the Guinea pig -- so far all else is unaffected.

--- Apps/ClickMe/projects_dir/iOS/moc_mainwindow.cpp
+++ Apps/ClickMe/projects_dir/iOS/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "mainwindow.h"
+#include "../../mainwindow.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
