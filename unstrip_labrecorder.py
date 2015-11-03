6c3aee5ded453c891b28f7cd8045c7ee0dadd3d5
David Medine <dmedine@ucsd.edu>
2015-11-03 09:37:06 -0800

    script showing use of lsl_get_deps.py for one app

---
+++ unstrip_labrecorder.py
@@ -0,0 +1,21 @@
+# example showing prcdedure for unstripping just one app
+import lsl_get_deps as lgd
+import os
+
+# define the current operating system we want
+op_sys = ["win32"]  # which os to unstrip -- win32 is the only one fully supported
+
+# which apps are to be stripped/unstripped
+apps = [
+    "LabRecorder/src",
+    "LabRecorder/src/pylsl",
+    "LabRecorder/src/TestRecorder"]
+
+# needed to obtain local copies of liblsl
+lgd.dl_deps(lgd.libs, lgd.libs_d, op_sys)
+
+# and the non liblsl-libraries for these apps
+lgd.dl_deps(apps, lgd.apps_d, op_sys)
+
+# bring the libs into selected apps
+lgd.unstrip(apps, lgd.apps_d, lgd.apps_dir, op_sys)
