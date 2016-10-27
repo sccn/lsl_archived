dc0e530d8b5a083a268faaeb2aa68f132c2ee864
David Medine <dmedine@ucsd.edu>
2016-10-27 15:57:24 -0700

    fixed loop exit and removed lag factor from XDF

--- Apps/BioSemi/ui_mainwindow.h
+++ Apps/BioSemi/ui_mainwindow.h
@@ -1,8 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Mon May 11 13:05:09 2015
-**      by: Qt User Interface Compiler version 4.8.1
+** Created by: Qt User Interface Compiler version 4.8.6
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
