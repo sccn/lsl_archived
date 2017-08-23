1c8e86a1c655d69870723914ccde1ddac71ac53c
Chadwick Boulay <chadwick.boulay@gmail.com>
2017-08-23 14:57:55 -0400

    LabRecorder cmake: Fixed library link order; cmake apps: Fixed windeployqt; cmake: changed default for LabRecorder and XDFBrowser to On

--- cmake/LSLAppBoilerplate.cmake
+++ cmake/LSLAppBoilerplate.cmake
@@ -93,16 +93,55 @@ function(installLSLApp target)
 	# do we need to install with Qt5?
 	get_target_property(TARGET_LIBRARIES ${target} LINK_LIBRARIES)
 	if(";${TARGET_LIBRARIES}" MATCHES ";Qt5::")
-		install(CODE "message(STATUS \"Please run windeployqt / macdeployqt manually until we figure out how to make CMake do it\")")
 		# TODO: add the executable name to a list we feed to *deployqt later on?
-
-		#set(libdir "${CMAKE_INSTALL_PREFIX}/LSL/lib")
-		#set(appbin "${CMAKE_INSTALL_PREFIX}/${target}/${target}${CMAKE_EXECUTABLE_SUFFIX}")
-		#install(CODE "(TARGET foo POST_BUILD
-		#	COMMAND ${CMAKE_COMMAND} -E remove_directory "${CMAKE_CURRENT_BINARY_DIR}/windeployqt"
-		#	COMMAND set PATH=%PATH%$<SEMICOLON>${qt5_install_prefix}/bin
-		#	COMMAND Qt5::windeployqt "--libdir ${libdir}" ${appbin}
-		# )
+		# https://gitlab.pluribusgames.com/games/apitrace/blob/master/gui/CMakeLists.txt#L121-174
+		if(WIN32)
+			#install(CODE "(TARGET foo POST_BUILD
+			#	COMMAND ${CMAKE_COMMAND} -E remove_directory "${CMAKE_CURRENT_BINARY_DIR}/windeployqt"
+			#	COMMAND set PATH=%PATH%$<SEMICOLON>${qt5_install_prefix}/bin
+			#	COMMAND Qt5::windeployqt "--libdir ${libdir}" ${appbin}
+			# )
+			get_target_property (QT_QMAKE_EXECUTABLE Qt5::qmake IMPORTED_LOCATION)
+			get_filename_component (QT_BIN_DIR "${QT_QMAKE_EXECUTABLE}" DIRECTORY)
+			find_program (QT_WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${QT_BIN_DIR}")
+			if (QT_WINDEPLOYQT_EXECUTABLE)
+				file (TO_NATIVE_PATH "${QT_BIN_DIR}" QT_BIN_DIR_NATIVE)
+				# It's safer to use `\` separators in the Path, but we need to escape them
+				string (REPLACE "\\" "\\\\" QT_BIN_DIR_NATIVE "${QT_BIN_DIR_NATIVE}")
+				
+				# windeployqt needs VCINSTALLDIR to copy MSVC Runtime files, but it's
+				# usually not define with MSBuild builds.
+				if ($ENV{VCINSTALLDIR})
+					set (VCINSTALLDIR "$ENV{VCINSTALLDIR}")
+				elseif (MSVC12)
+					set (VCINSTALLDIR "\$ENV{VS120COMNTOOLS}/../../VC")
+				elseif (MSVC14)
+					set (VCINSTALLDIR "\$ENV{VS140COMNTOOLS}/../../VC")
+				else ()
+					message (FATAL_ERROR "Unsupported MSVC version ${MSVC_VERSION}")
+				endif ()
+		
+				set(appbin "${CMAKE_INSTALL_PREFIX}/${target}/${target}${CMAKE_EXECUTABLE_SUFFIX}")
+				install (CODE "
+					message (STATUS \"Running Qt Deploy Tool...\")
+					set(QT_WINDEPLOYQT_FLAGS --no-translations --no-system-d3d-compiler --no-opengl-sw)
+					if (CMAKE_INSTALL_CONFIG_NAME STREQUAL \"Debug\")
+						list (APPEND QT_WINDEPLOYQT_FLAGS --debug)
+					else ()
+						list (APPEND QT_WINDEPLOYQT_FLAGS --release)
+					endif ()
+					execute_process(COMMAND
+						\"${CMAKE_COMMAND}\" -E env
+						\"Path=${QT_BIN_DIR_NATIVE};\$ENV{SystemRoot}\\\\System32;\$ENV{SystemRoot}\"
+						\"VCINSTALLDIR=${VCINSTALLDIR}\"
+						\"${QT_WINDEPLOYQT_EXECUTABLE}\"
+						\${QT_WINDEPLOYQT_FLAGS}
+						\"${appbin}\"
+					)
+				")
+			endif(QT_WINDEPLOYQT_EXECUTABLE)
+		elseif(APPLE)
+			install(CODE "message(STATUS \"Please run macdeployqt manually until we figure out how to make CMake do it\")")
+		endif(WIN32)
 	endif()
 endfunction()
-
