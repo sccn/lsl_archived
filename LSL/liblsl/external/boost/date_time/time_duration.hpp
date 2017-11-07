ba970d5ff6832b9e5224dc101a8885fc9c31b909
Tristan Stenner <ttstenner@gmail.com>
2017-11-07 16:39:10 +0100

    Add lslboost headers

---
+++ LSL/liblsl/external/boost/date_time/time_duration.hpp
@@ -0,0 +1,6 @@
+#include <lslboost/date_time/time_duration.hpp>
+
+#ifndef LSLBOOST_NAMESPACE_DECLARED
+#define LSLBOOST_NAMESPACE_DECLARED
+namespace lslboost { }; namespace boost = lslboost;
+#endif
